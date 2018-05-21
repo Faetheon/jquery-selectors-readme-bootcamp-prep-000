c1f951684dc0ac5abd6a15b4928f74ca9dc7258c
Nathan Hill <nateohill@gmail.com>
2018-05-21 17:28:37 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,11 @@
   function lastImageSelector() {
     return $('img:last');
   }
+  
+  function ninjaBabySelector() {
+    return $('#ninjaBaby');
+  }
+  
+  function divSelector() {
+    return $('div.pics');
+  }
