11b51f0886f1d545e58e75c6a12e1cd37c0c8d76
Nathan Hill <nateohill@gmail.com>
2018-05-21 17:23:45 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,9 @@
-// declare your functions here...
+$(document).ready(function() {
+  function paragraphSelector() {
+   return $('p');
+  }
+  
+  function lastImageSelector() {
+    return $('img').pop();
+  }
+})
